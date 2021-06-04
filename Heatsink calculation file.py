8b9e6e9074d8dcfc49dd7bb4f04a26f053acbb13
varnaugj <varnaugj@yahoo.com>
2021-06-04 17:00:22 -0400

    updates

---
+++ Heatsink calculation file.py
@@ -0,0 +1,6 @@
+#heatsink calculation file
+
+import math
+import scipy 
+from scipy import constants as const 
+
--- machinelearn2.py
+++ machinelearn2.py
@@ -4,12 +4,19 @@
 #from nltk import send_tokenize
 #from nltk import word_tokenize
 import requests
-from bs4 import BeautifulSoup
+import newspaper
+from bs4 import BeautifulSoup as bs
 from newspaper import Article
 
 def news():
     #target we want to open 
-    url = 'https://www.cnn.com/travel/article/qantas-california-desert-a380s-rattlesnakes/index.html'
+    url = 'https://www.cnn.com'
+    
+    papertest = newspaper.build(url)
+
+    for article in papertest.articles:
+        print(article.url)
+
 
     #open with GET method
     resp = requests.get(url)
@@ -18,24 +25,45 @@ def news():
         print("Successfully opened the web page")
         print("the news are as follow :-\n")
 
-        article = Article(url)
-        article.download()
+    #    article = Article(url)
+    #    article.download()
 
-        article.parse()
-        article.nlp()
-        text = article.text
+    #    article.parse()
+    #    article.nlp()
+    #    text = article.text
 
-        print (type(text))
-        print("\n")
+    #    print (type(text))
+    #    print("\n")
 
-        print(text)
-        print("\n")
-        print(len(text))
-        print(article.keywords)
+    #    print(text)
+    #    print("\n")
+    #    print(len(text))
+    #    print(article.keywords)
         
     else:
         print("Error")
-news()
+#news()
+
+
+def news2():
+    #url = 'https://www.cnn.com/politics'
+    url = 'https://www.foxnews.com/politics'
+    #open with GET method
+    resp = requests.get(url)
+
+    soup = bs(resp.text, 'html.parser')
+    rawtext = soup.get_text()
+
+    #http_respone 200 means link works
+    if resp.status_code==200:
+        print("Successfully opened the web page")
+        print("the news are as follow :-\n")
+        for link in soup.find_all('a'):
+            print(link.get('href'))
+    
+    else:
+        print("Error")
 
 
 
+news2()
